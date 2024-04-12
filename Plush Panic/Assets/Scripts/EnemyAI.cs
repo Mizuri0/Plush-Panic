132f3416642d06c0d2308447c3129d3cf1142ab8
Mizuri0 <2Hung.H2@gmail.com>
2024-04-12 13:49:24 -0700

    Blah Blah (4/12/2024)

--- Plush Panic/Assets/Scripts/EnemyAI.cs
+++ Plush Panic/Assets/Scripts/EnemyAI.cs
@@ -1,6 +1,7 @@
 
 using UnityEngine;
 using UnityEngine.AI;
+public GameObject projectile;
 
 public class EnemyAItutorial : MonoBehaviour
 { 
@@ -9,6 +10,8 @@ public class EnemyAItutorial : MonoBehaviour
     public Transform player;
 
     public LayerMask whatIsGround, whatIsPlayer;
+
+    public float health;
 
     //Patroling
     public Vector3 walkPoint;
@@ -43,6 +46,15 @@ public class EnemyAItutorial : MonoBehaviour
     private void Patroling() 
     { 
         if (!walkPointSet) SearchWalkPoint(); 
+
+        if (walkPointSet) 
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint; 
+
+        //Walkpoint reached 
+        if (distanceToWalkPoint.magnitude < 1f) 
+            walkPointSet = false;
     } 
     private void SearchWalkPoint() 
     { 
@@ -50,9 +62,60 @@ public class EnemyAItutorial : MonoBehaviour
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.y, transform.position.z + randomZ)
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.y, transform.position.z + randomZ);
 
         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
             walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position); 
+    }
+    private void AttackPlayer()
+    { 
+        //Make sure enemy doesn't move 
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player); 
+        
+        if (!alreadyAttacked)
+        { 
+            ///Attack code here 
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            r.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+            ///
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+
+        }
+    }
+    private void ResetAttack()
+    { 
+        alreadyAttacked = false;
+    } 
+    
+    public void TakeDamage(int damage) 
+    {
+        health -= damage;
+        
+        if (health <= 0) Invoke(nameof(DestroyEnemy), .5f);
+        
+    }
+    private void DestroyEnemy() 
+    { 
+        Destroy(gameObject); 
+    }
+
+    private void OnDrawGizmosSelected()
+    { 
+        Gizmos.color = Color.Red; 
+        Gizmos.DrawWireShere(transform.position, attackRange); 
+        Gizmos.color = Color.yellow; 
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
