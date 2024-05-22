776d2c3ae4c1c29cfd76d89f6ff527417c288bd6
Mizuri0 <145395524+Mizuri0@users.noreply.github.com>
2024-05-22 13:06:44 -0700

    Blah Blah (5/22/2024)

--- Plush Panic/Assets/Scripts/InventoryItem.cs
+++ Plush Panic/Assets/Scripts/InventoryItem.cs
@@ -1,25 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.UI; 
+using UnityEngine.EventSystems; 
 
-public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler 
 {
+    [Header("UI")] 
+    public Image image;
 
+    [HideInInspector] public Transform parentAfterDrag;
 
     // Drag and drop 
-    public void OnBeginDrag(PointerEventData evenData) 
-    { 
-        image.raycastTarget = false;
-    }
-
-    public void OnDrag(PointerEventData evenData)
+    public void OnBeginDrag(PointerEventData eventData) 
     {
-        transform.position = Input.mousePosition;
-    }
+        image.raycastTarget = false; 
+        parentAfterDrag = transform.parent; 
+        transform.SetParent(transform.root);
+    } 
+        
+        public void OnDrag(PointerEventData eventData) 
+        {
+            transform.position = Input.mousePosition; 
+        }
 
-    public void OnEndDrag(PointerEventData evenData) 
-    {
-        image.raycastTarget = true;
-    }
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            image.raycastTarget = true; 
+            transform.SetParent(parentAfterDrag);
+        }
 }
+
+
+
+
+
