55021aa163f9be0709f11eaf5b1c9d32f49e3699
Mizuri0 <145395524+Mizuri0@users.noreply.github.com>
2024-05-31 13:49:19 -0700

    Blah Blah (5/31/2024)

--- Plush Panic/Assets/Scripts/InventoryItem.cs
+++ Plush Panic/Assets/Scripts/InventoryItem.cs
@@ -1,29 +1,33 @@
-using System.Collections; 
-using System.Collections.Generic; 
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
+
 
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public Image image; 
+    public Image image;
     [HideInInspector] public Transform parentAfterDrag;
 
+
     public void OnBeginDrag(PointerEventData eventData) {
-        Debug. Log("Begin drag"); 
-        parentAfterDrag = transform.parent; 
-        transform.SetParent(transform.root); 
+        Debug. Log("Begin drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
         transform.SetAsLastSibling();
         image.raycastTarget = false;
     }
 
+
     public void OnDrag(PointerEventData eventData) {
-        Debug. Log("Dragging"); 
+        Debug. Log("Dragging");
         transform.position = Input.mousePosition;
     }
 
+
     public void OnEndDrag (PointerEventData eventData) {
-        Debug.Log("End drag"); 
+        Debug.Log("End drag");
         transform.SetParent(parentAfterDrag);
         image.raycastTarget = true;
     }
