6f8fe1f8cee1af799e720598b48b43f56037bd38
Mizuri0 <145395524+Mizuri0@users.noreply.github.com>
2024-06-06 13:49:29 -0700

    Blah Blah (6/6/2024)

--- Plush Panic/Assets/Scripts/InventorySlot.cs
+++ Plush Panic/Assets/Scripts/InventorySlot.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InventorySlot : MonoBehaviour, IDropHandler
+
+public class YourClass : MonoBehaviour, IDropHandler
 {
-    public void OnDrop(PointerEventData eventData)
-    {
-        if (transform.childCount == 0)
-        {
-            GameObject dropped = eventData.pointerDrag;
-            DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
-            draggableItem.parentAfterDrag = transform;
+    public void OnDrop(PointerEventData eventData) {
+        if (transform.childCount == 0) {
+        GameObject dropped = eventData.pointerDrag;
+        DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+        draggableItem.parentAfterDrag = transform;
         }
     }
+
+
 }
